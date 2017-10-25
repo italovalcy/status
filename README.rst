b0ba6078711f90aa128d9ff6b3973ce0b758f92e
Beraldo Leal <beraldo@cern.ch>
2017-10-25 08:42:43 -0200

    first commit

---
+++ README.rst
@@ -0,0 +1,36 @@
+########
+Overview
+########
+
+.. attention::
+
+    THIS NAPP IS STILL EXPERIMENTAL AND IT'S EVENTS, METHODS AND STRUCTURES MAY
+    CHANGE A LOT ON THE NEXT FEW DAYS/WEEKS, USE IT AT YOUR OWN DISCERNEMENT
+
+The *kytos/status* application provides basic information about the running
+controller.
+
+It is a really simple NApp.
+
+##########
+Installing
+##########
+
+All of the Kytos Network Applications are located in the NApps online
+repository. To install this NApp, run:
+
+.. code:: shell
+
+   $ kytos napps install kytos/status
+
+########
+Rest API
+########
+
+You can find a list of the available endpoints and example input/output in the
+'REST API' tab in this NApp's webpage in the `Kytos NApps Server
+<https://napps.kytos.io/kytos/status>`_.
+
+.. note::
+
+    Working in progress here.
---
+++ main.py
@@ -0,0 +1,60 @@
+"""Main module of kytos/status Kytos Network Application.
+
+Provides basic information about the controller status
+"""
+
+from kytos.core import KytosNApp, log, rest
+
+from napps.kytos.status import settings
+
+import json
+
+class Main(KytosNApp):
+    """Main class of kytos/status NApp.
+
+    This class is the entry point for this napp.
+    """
+
+    def setup(self):
+        """Replace the '__init__' method for the KytosNApp subclass.
+
+        The setup method is automatically called by the controller when your
+        application is loaded.
+
+        So, if you have any setup routine, insert it here.
+        """
+        pass
+
+    def execute(self):
+        """This method is executed right after the setup method execution.
+
+        You can also use this method in loop mode if you add to the above setup
+        method a line like the following example:
+
+            self.execute_as_loop(30)  # 30-second interval.
+        """
+        pass
+
+    @rest('v1/')
+    def get_controller_status(self):
+        status = self.controller.status()
+        return json.dumps({'status': status})
+
+    @rest('v1/napps')
+    def get_napps_info(self):
+        napps = [ napp[1] for napp in self.controller.napps.items() ]
+        output = []
+        for napp in napps:
+            output.append({'name': "{}/{}".format(napp.author, napp.name),
+                           'version': napp.version,
+                           'description': napp.description,
+                           'license': napp.license,
+                           'tags': napp.tags})
+        return json.dumps({'napps': output}), 200
+
+    def shutdown(self):
+        """This method is executed when your napp is unloaded.
+
+        If you have some cleanup procedure, insert it here.
+        """
+        pass

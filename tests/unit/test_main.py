761fc9802ef4221500f149c8ad497b04bcaa7492
Gleyberson Andrade <gleybersonandrade@gmail.com>
2020-05-13 11:08:11 -0300

    Fix linter issues
    This commit changes known_first_party and known_third_party for setup.cfg file, and fixes some linter import issues.

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -2,7 +2,7 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
-from tests.helpers import get_controller_mock, get_test_client
+from kytos.lib.helpers import get_controller_mock, get_test_client
 
 
 class TestMain(TestCase):
@@ -18,17 +18,15 @@ class TestMain(TestCase):
 
         self.napp = Main(get_controller_mock())
 
-    @patch('kytos.core.Controller.status')
-    def test_get_controller_status(self, mock_status):
+    @patch('kytos.core.Controller.status', return_value='status')
+    def test_get_controller_status(self, _):
         """Test get_controller_status method."""
-        mock_status.return_value = 'status'
         api = get_test_client(self.napp.controller, self.napp)
-
         url = "%s/v1/" % self.API_URL
         response = api.open(url, method='GET')
 
-        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {'status': 'status'})
+        self.assertEqual(response.status_code, 200)
 
     def test_get_napps_info(self):
         """Test get_napps_info method."""
@@ -43,12 +41,11 @@ class TestMain(TestCase):
             napp.tags = 'tags'
             napps.append(napp)
             self.napp.controller.napps.update({index: napp})
-        api = get_test_client(self.napp.controller, self.napp)
 
+        api = get_test_client(self.napp.controller, self.napp)
         url = "%s/v1/napps" % self.API_URL
         response = api.open(url, method='GET')
 
-        self.assertEqual(response.status_code, 200)
         response_napps = response.json['napps']
         for index, napp in enumerate(napps):
             response_napp = response_napps[index]
@@ -57,3 +54,4 @@ class TestMain(TestCase):
             self.assertEqual(response_napp['description'], 'description')
             self.assertEqual(response_napp['license'], 'license')
             self.assertEqual(response_napp['tags'], 'tags')
+        self.assertEqual(response.status_code, 200)
